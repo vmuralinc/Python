74da5ff1819fdb17d5509cfb3f4d33fd2c4a26b2
vmuralinc@gmail.com <vmuralinc@gmail.com>
2018-05-10 00:09:40 -0700

    added more test cases

--- tests/tests_sllnode.py
+++ tests/tests_sllnode.py
@@ -6,10 +6,26 @@ class SllNodeTestSuite(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_create_node_with_int_and_none(self):
+    def test_create_node_with_int_value(self):
         self.node = datastructures.SllNode(5)
         self.assertEqual(self.node.value, 5)
         self.assertEqual(self.node.next, None)
+    
+    def test_create_node_with_string_value(self):
+        self.node = datastructures.SllNode("abc")
+        self.assertEqual(self.node.value, "abc")
+        self.assertEqual(self.node.next, None)
+    
+    def test_create_node_with_none_value(self):
+        self.assertRaises(ValueError, datastructures.SllNode, None)
+        
+    def test_repr_node_with_int_value(self):
+        self.node = datastructures.SllNode(5)
+        self.assertEqual(repr(self.node.value), "5")
+    
+    def test_repr_node_with_string_value(self):
+        self.node = datastructures.SllNode("abc")
+        self.assertEqual(repr(self.node.value), repr("abc"))
 
 if __name__ == '__main__':
     unittest.main()
